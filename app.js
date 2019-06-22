40595cb369d568d5fee0bd4d956e967c23c6d577
Maria Siedlik <seo.ktoredziala@gmail.com>
2019-06-22 21:55:19 +0200

    funkcje java script

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 console.log('elo');
-$name = 'Maria Siedlik';
-$age = '33';
+const name = 'Maria Siedlik';
+const age = '33';
 
-console.log('Nazywam się ' + $name + ' mam ' + $age + ' lata.');
+// console.log('Nazywam się ' + $name + ' mam ' + $age + ' lata.');
+console.log(`Nazywam się ${name}  mam ${age} lata.`);
 
 const heading = document.querySelector('.header__heading');
 
@@ -26,3 +27,20 @@ console.log(heading.innerHTML);
 
 const emptyParagraph = document.querySelector('.empty-paragraph');
 emptyParagraph.innerHTML = 'HALO! JEST TU KTOŚ? zminiłam zawartość tego elementu aaa';
+
+//LOGIKA JAVA SCRIPT
+const myAge = 40;
+
+switch (myAge) {
+   case 20:
+      console.log('masz 20 lat');
+      break;
+   case 30:
+      console.log('masz 30 lat');
+      break;
+   case 40:
+      console.log('Masz 40 lat');
+      break;
+   default:
+      console.log('ddd');
+}
